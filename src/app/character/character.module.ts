bdeca7c2252dcde3ac925d567460e6fd71ff7ade
lukaszbros <lukasz.bros@gmail.com>
2021-05-18 17:42:59 +0200

    Pagination as form

--- src/app/character/character.module.ts
+++ src/app/character/character.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from "@angular/core";
 import { CharacterComponent } from "./character.component";
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSelectModule } from "@angular/material/select";
+import { MatOptionModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
       CharacterComponent
   ], imports: [
+    CommonModule,
     MatTableModule,
     MatPaginatorModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule
   ]
 })
 export class CharacterModule {
