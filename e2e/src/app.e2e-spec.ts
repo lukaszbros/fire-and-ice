968aee7f3c1e18202ebaaba4f59d34212e7e6a99
lukaszbros <lukasz.bros@gmail.com>
2021-05-20 19:05:11 +0200

    Improve basic tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('fire-and-ice app is running!');
+    expect(page.getTitleText()).toEqual('Fire and Ice Book of Knowledge');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fire-and-ice app is running!');
+    expect(compiled.querySelector('mat-toolbar span').textContent).toContain('Fire and Ice Book of Knowledge');
   });
 });
